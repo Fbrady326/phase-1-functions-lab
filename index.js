ef636d8745797e972ec156161fbb65eebd82399e
BradyFoster <Bjfoster9860@gmail.com>
2023-05-17 13:08:35 -0700

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    const hq = 42;
+    const distance = Math.abs(block - hq);
+    return distance;
+  };
+
+  function distanceFromHqInFeet(feet) {
+    let blockDistanceInFeet = 264;
+    return distanceFromHqInBlocks(feet) * blockDistanceInFeet
+}
+
+  function distanceTravelledInFeet(start, destination) {
+    const blockDistanceInFeet = 264;
+    const distance = Math.abs(destination - start);
+    return distance * blockDistanceInFeet;
+  };
+
+  function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    let price = 0;
+    const setPrice = 2500;
+    if (distance <= 400) {
+      price = 0;
+      return price;
+    } else if (distance <= 2000) {
+      price = distance * 2 / 100;
+      return 2.56;
+    } else if (distance <= 2500) {
+      price = setPrice/100;
+      return price;
+    } else {
+      return 'cannot travel that far';
+    }
+  };
